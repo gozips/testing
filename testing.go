e06bc52bfc4cf9679dfa0cd2ce7d27b625417325
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-11-30 21:23:49 -0500

    Return any errors during verification

--- testing.go
+++ testing.go
@@ -6,11 +6,13 @@ import "testing"
 import "github.com/nowk/assert"
 
 // tZipReader unzips []byte and returns the zip.Reader for the expanded zip
-func tZipReader(b []byte) *zip.Reader {
+func tZipReader(b []byte) (*zip.Reader, error) {
 	r := bytes.NewReader(b)
-	z, _ := zip.NewReader(r, int64(r.Len()))
-
-	return z
+	z, err := zip.NewReader(r, int64(r.Len()))
+	if err != nil {
+		return nil, err
+	}
+	return z, nil
 }
 
 // Entries is a simple struct for an entry's name and body contents
@@ -19,16 +21,29 @@ type Entries struct {
 }
 
 // VerifyZip asserts the contents of a zip against an []tTable
-func VerifyZip(t *testing.T, b []byte, entries []Entries) {
-	z := tZipReader(b)
+func VerifyZip(t *testing.T, b []byte, entries []Entries) error {
+	z, err := tZipReader(b)
+	if err != nil {
+		return err
+	}
+
 	for i, entry := range entries {
 		f := z.File[i]
-		rc, _ := f.Open()
-		defer rc.Close()
+		r, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer r.Close()
 
 		b := make([]byte, 32*1024)
-		n, _ := rc.Read(b)
+		n, err := r.Read(b)
+		if err != nil {
+			return err
+		}
+
 		assert.Equal(t, entry.Name, f.Name)
 		assert.Equal(t, entry.Body, string(b[:n]))
 	}
+
+	return nil
 }
